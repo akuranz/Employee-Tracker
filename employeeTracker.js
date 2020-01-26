331d7423c9313ce190f6c21df39369c0880d65ca
Abby Kuranz <akuranz@gmail.com>
2020-01-26 16:04:05 -0700

    added prompts to remove and add

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,7 +2,6 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 const cTable = require("console.table");
 
-// create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -11,14 +10,12 @@ var connection = mysql.createConnection({
   database: "employeeTracker_DB"
 });
 
-// connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
   start();
 });
 
-// function which prompts the user for what action they should take
 const start = () => {
   inquirer
     .prompt({
@@ -28,7 +25,10 @@ const start = () => {
       choices: [
         "View All Employees",
         "View All Employees by Department",
-        "View All Employees by Manager"
+        "View All Employees by Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role"
       ]
     })
     .then(({ userInput }) => {
@@ -39,6 +39,12 @@ const start = () => {
           return returnAllEmpDept();
         case "View All Employees by Manager":
           return returnAllEmpMngr();
+        case "Add Employee":
+          return addEmp();
+        case "Remove Employee":
+          return removeEmp();
+        case "Update Employee Role":
+          return updateEmpRole();
       }
       connection.end();
     });
@@ -47,13 +53,18 @@ const start = () => {
 const returnAllEmp = () => {
   console.log("Return All Employees");
 
-  connection.query("SELECT * FROM employees", function(err, res) {
-    if (err) throw err;
-    // Log all results of the SELECT statement
-    // console.log(res);
-    const table = cTable.getTable(res);
-    console.log(`\n${table}`);
-  });
+  query = connection.query(
+    // "SELECT employees.id, employees.first_name, employees.last_name, role.title FROM employees JOIN roles ON employees.role_id = roles.id",
+    "SELECT id, first_name, last_name FROM employees",
+    // "SELECT title FROM roles",
+    function(err, res) {
+      if (err) throw err;
+      // console.log(res);
+      const table = cTable.getTable(res);
+      console.log(`\n${table}`);
+    }
+  );
+  // console.log(query.sql);
 
   start();
 };
@@ -65,5 +76,72 @@ const returnAllEmpDept = () => {
 
 const returnAllEmpMngr = () => {
   console.log("Return All Employees by Manager");
+  start();
+};
+
+const removeEmp = () => {
+  console.log("Remove an Employee");
+  connection.query("SELECT * FROM employees", function(err, res) {
+    if (err) throw err;
+    inquirer.prompt([
+      {
+        type: "list",
+        message: "Which employee do you want to remove?",
+        name: "employeeName",
+        choices: function() {
+          var employeeArray = [];
+          for (var i = 0; i < res.length; i++) {
+            employeeArray.push(res[i].first_name + " " + res[i].last_name);
+          }
+          return employeeArray;
+        }
+      }
+    ]);
+    start();
+  });
+};
+
+const addEmp = () => {
+  console.log("Add an Employee");
+  //join with manager to return manager list for last question
+  connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+    inquirer.prompt([
+      {
+        type: "input",
+        message: "What is your employee's first name?",
+        name: "empFName"
+      },
+      {
+        type: "input",
+        message: "What is your employee's last name?",
+        name: "empLName"
+      },
+      {
+        type: "list",
+        message: "What is your employee's role?",
+        name: "empRole",
+        choices: function() {
+          var roleArray = [];
+          for (var i = 0; i < res.length; i++) {
+            roleArray.push(res[i].title);
+          }
+          return roleArray;
+        }
+      },
+      {
+        type: "input",
+        message: "Who is your employee's manager?",
+        name: "empMngr"
+        //Need to add choices array
+      }
+    ]);
+    //.then(add function from a class that adds employees)
+    start();
+  });
+};
+
+const updateEmpRole = () => {
+  console.log("Update an Employee");
   start();
 };
--- employeeTracker_DB.sql
+++ employeeTracker_DB.sql
@@ -3,21 +3,14 @@ CREATE DATABASE employeeTracker_DB;
 
 USE employeeTracker_DB;
 
-CREATE TABLE employees(
-  id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  PRIMARY KEY (id)
-  -- FOREIGN KEY (role_id) REFERENCES roles(id)
-);
-
 CREATE TABLE departments(
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(30),
   PRIMARY KEY (id)
 );
+
+INSERT INTO departments (name)
+VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
 CREATE TABLE roles(
   id INT NOT NULL AUTO_INCREMENT,
@@ -28,18 +21,31 @@ CREATE TABLE roles(
   FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
-INSERT INTO employees (first_name, last_name)
-VALUES ("John", "Doe"), ("Mike", "Chan"), ("Ashley", "Rodriguez");
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Sales Lead", "100000", 1), ("Salesperson", "80000", 1), ("Lead Engineer", "150000", 2), ("Software Engineer", "120000", 2), ("Accountant", "125000", 3), ("Legal Team Lead", "250000", 4), ("Lawyer", "190000", 4);
 
-INSERT INTO departments (name)
-VALUES ("Sales"), ("Engineering"), ("Finance"), ("Legal");
+CREATE TABLE employees(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES roles(id)
+);
 
-INSERT INTO roles (title, salary)
-VALUES ("Sales Lead", "100000"), ("Salesperson", "80000"), ("Lead Engineer", "150000"), ("Software Engineer", "120000"), ("Accountant", "125000"), ("Legal Team Lead", "250000"), ("Lawyer", "190000");
+CREATE TABLE managers(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  PRIMARY KEY (id)
+);
 
--- ALTER TABLE roles ADD FOREIGN KEY department_id (department_id) REFERENCES roles(id);
+INSERT INTO employees (first_name, last_name, role_id)
+VALUES ("John", "Doe", 1), ("Mike", "Chan", 2), ("Ashley", "Rodriguez", 3), ("Kevin", "Tupik", 4), ("Malia", "Brown", 5), ("Sarah", "Lourd", 6), ("Tom", "Allen", 7), ("Christian", "Eckinrode", 3);
 
 
-SELECT * FROM employees;
 SELECT * FROM departments;
 SELECT * FROM roles;
+SELECT * FROM employees;
+SELECT * FROM managers;
